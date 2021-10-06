a3a85eb5da615a1623e85f4f5c8f86651965a195
Thomas Moraes <thomas.moraes@semexe.com>
2021-10-05 21:27:22 -0300

    Bug de admin corrigido

--- src/config/config.php
+++ src/config/config.php
@@ -3,17 +3,17 @@
 // Configurações para acesso ao banco de dados
 define("DB_DRIVER", "mysql");
 
-// define("DB_HOST", "127.0.0.1");
-define("DB_HOST", "awsmysqlserver.cpmmtmvm6bii.us-east-2.rds.amazonaws.com");
+define("DB_HOST", "127.0.0.1");
+// define("DB_HOST", "awsmysqlserver.cpmmtmvm6bii.us-east-2.rds.amazonaws.com");
 
-// define("DB_NAME", "users");
-define("DB_NAME", "test_database_aws");
+define("DB_NAME", "users");
+// define("DB_NAME", "test_database_aws");
 
-// define("DB_USER", "root");
-define("DB_USER", "admin");
+define("DB_USER", "root");
+// define("DB_USER", "admin");
 
-// define("DB_PASS", "");
-define("DB_PASS", "aws123456");
+define("DB_PASS", "");
+// define("DB_PASS", "aws123456");
 
 
 // Remova a última barra "/" da URL
