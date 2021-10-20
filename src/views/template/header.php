e3d7cba5c66b1a4f72a0c6c1ce2d990d6a2b00b4
Thomas Moraes <thomas.moraes@semexe.com>
2021-10-20 19:39:02 -0300

    Incluindo favicon no site

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -12,6 +12,10 @@
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="apple-touch-icon" sizes="180x180" href="public/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="public/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="public/img/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 
 <nav>
